f2a51676191c0ec4315d690a14c668a96ed29285
jvk59 <vivek-cs059@dsce.edu.in>
2022-08-10 09:17:51 +0530

    paragraph text features

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function showNotes() {
     html += `
             <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title" id="note-priority">Note ${index + 1}</h5>
+                        <h5 class="card-title" id="note-priority">Note ${index + 1} :-</h5>
                         <p class="card-text" id="notes-desc"> ${element}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-danger">Delete Note</button>
                     </div>
